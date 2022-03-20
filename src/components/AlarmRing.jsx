2cbbba2daca7d4a8dfa79f9e1750fc3638dadb39
Gudine <gudine283@gmail.com>
2022-03-20 14:23:25 -0300

    feat: Ajusta assets e adiciona audio

--- src/components/AlarmRing.jsx
+++ src/components/AlarmRing.jsx
@@ -1,5 +1,7 @@
 import React, { Component, createRef } from 'react';
 import './AlarmRing.css';
+import checkpointSnd from '../snd/checkpoint.wav';
+import endSnd from '../snd/end.wav';
 
 class AlarmRing extends Component {
   constructor() {
@@ -16,6 +18,7 @@ class AlarmRing extends Component {
       const curr = alarms[index];
       if (prev !== curr && curr === true) {
         overlay = true;
+        this.alarmRefs[index].current.children[0].play();
         this.alarmRefs[index].current.animate(
           [
             { opacity: 0, width: 0 },
@@ -42,7 +45,12 @@ class AlarmRing extends Component {
       <>
         <div className="red-fill" ref={ this.overlayRef } />
         {Array(10).fill().map((_, i) => (
-          <div key={ i } ref={ this.alarmRefs[i] } className="alarm-ring" />
+          <div key={ i } ref={ this.alarmRefs[i] } className="alarm-ring">
+            <audio src={ checkpointSnd }>
+              Your browser does not support the
+              <code>audio</code> element.
+            </audio>
+          </div>
         ))}
       </>
     );
